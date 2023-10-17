29204768094694919f62d99317a9095331c498d8
Alejo Boga <alejoboga20@gmail.com>
2023-10-17 10:21:29 -0300

    send correct pokemon

--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,39 +1,53 @@
 <template>
 	<main class="game">
-		<h1>Who's that pokémon?</h1>
-		<PokemonImage :pokemonId="6" :showPokemon="false" />
-		<PokemonOptions :pokemons="pokemonNamesArray" />
+		<div v-if="!pokemon">
+			<h1>Loading...</h1>
+		</div>
+		<div v-else="pokemon">
+			<h1>Who's that pokémon?</h1>
+			<PokemonImage :pokemonId="pokemon.id" :showPokemon="showPokemon" />
+			<PokemonOptions :pokemons="pokemonNamesArray" />
+		</div>
 	</main>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 import PokemonImage from '@/components/PokemonImage.vue';
 import PokemonOptions from '@/components/PokemonOptions.vue';
 
 import getPokemonOptions from '../helpers/getPokemonOptions';
 
-export default {
+interface Pokemon {
+	id: number;
+	name: string;
+}
+
+export default defineComponent({
 	name: 'PokemonPage',
 	components: {
 		PokemonImage,
 		PokemonOptions,
 	},
 	data() {
-		const pokemonNamesArray: { id: number; name: string }[] = [];
-
 		return {
-			pokemonNamesArray,
+			pokemonNamesArray: [] as Pokemon[],
+			pokemon: undefined as Pokemon | undefined,
+			showPokemon: false,
 		};
 	},
 	methods: {
 		async mixPokemonArray() {
-			this.pokemonNamesArray = await getPokemonOptions();
+			this.pokemonNamesArray = (await getPokemonOptions()) as Pokemon[];
+
+			const rndInt = Math.floor(Math.random() * 4);
+			this.pokemon = this.pokemonNamesArray[rndInt];
 		},
 	},
 	mounted() {
 		this.mixPokemonArray();
 	},
-};
+});
 </script>
 
 <style lang="css" scoped>
