6c6a73938df6d44e9a6997d325470146d850985f
Alejo Boga <alejoboga20@gmail.com>
2023-10-17 16:20:25 -0300

    test pokemon picture

--- src/components/tests/PokemonImage.spec.ts
+++ src/components/tests/PokemonImage.spec.ts
@@ -1,6 +1,9 @@
 import { shallowMount } from '@vue/test-utils';
 import PokemonImage from '../PokemonImage.vue';
 
+const POKE_ID = 1;
+const IMG_SRC = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${POKE_ID}`;
+
 describe('PokemonImage', () => {
 	test('should match the Snapshot', () => {
 		const wrapper = shallowMount(PokemonImage, {
@@ -9,6 +12,26 @@ describe('PokemonImage', () => {
 		expect(wrapper.html()).toMatchSnapshot();
 	});
 
-	test('should show the hidden image and pokemon', () => {});
-	test('should show the pokemon if showPokemon:true', () => {});
+	test('should show the hidden image and pokemon id 1', () => {
+		const wrapper = shallowMount(PokemonImage, {
+			props: { pokemonId: 1, showPokemon: false },
+		});
+		const [hiddenImage, visibleImage] = wrapper.findAll('img');
+
+		expect(hiddenImage.classes('hidden-pokemon')).toBeTruthy();
+		expect(hiddenImage.exists()).toBeTruthy();
+
+		expect(visibleImage).toBe(undefined);
+	});
+
+	test('should show the pokemon if showPokemon:true', () => {
+		const wrapper = shallowMount(PokemonImage, {
+			props: { pokemonId: 1, showPokemon: true },
+		});
+		const [_, visibleImage] = wrapper.findAll('img');
+
+		expect(visibleImage.exists()).toBeTruthy();
+		expect(visibleImage.classes('hidden-pokemon')).toBeFalsy();
+		expect(visibleImage.attributes('src')).toBe(IMG_SRC + '.svg');
+	});
 });
